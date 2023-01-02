e77569b9e9f3ac7f5ab75166eabe6e8d388ab2d5
Radouane Ezzaggab <77213796+RadouaneEzzaggab@users.noreply.github.com>
2023-01-02 10:24:23 +0100

    fix

--- index.js
+++ index.js
@@ -116,26 +116,129 @@ const TinderCard = React.forwardRef(
       }
     }))
 
+    const handleSwipeReleased = React.useCallback(
+      async (setSpringTarget, gesture) => {
+        // Check if this is a swipe
+        const dir = getSwipeDirection({
+          x: swipeRequirementType === 'velocity' ? gesture.vx : gesture.dx,
+          y: swipeRequirementType === 'velocity' ? gesture.vy : gesture.dy
+        })
 
+        if (dir !== 'none') {
+          if (flickOnSwipe) {
+            if (!preventSwipe.includes(dir)) {
+              if (onSwipe) onSwipe(dir)
+
+              await animateOut(swipeRequirementType === 'velocity' ? ({
+                x: gesture.vx,
+                y: gesture.vy
+              }) : (
+                normalize({ x: gesture.dx, y: gesture.dy }) // Normalize to avoid flicking the card away with super fast speed only direction is wanted here
+              ), setSpringTarget, swipeRequirementType)
+              if (onCardLeftScreen) onCardLeftScreen(dir)
+              return
+            }
+          }
+        }
+
+        // Card was not flicked away, animate back to start
+        animateBack(setSpringTarget)
+      },
+      [swipeRequirementType, flickOnSwipe, preventSwipe, onSwipe, onCardLeftScreen]
+    )
 
     let swipeThresholdFulfilledDirection = 'none'
 
-    // const gestureStateFromWebEvent = (ev, startPositon, lastPosition, isTouch) => {
-    //   let dx = isTouch ? ev.touches[0].clientX - startPositon.x : ev.clientX - startPositon.x
-    //   let dy = isTouch ? ev.touches[0].clientY - startPositon.y : ev.clientY - startPositon.y
+    const gestureStateFromWebEvent = (ev, startPositon, lastPosition, isTouch) => {
+      let dx = isTouch ? ev.touches[0].clientX - startPositon.x : ev.clientX - startPositon.x
+      let dy = isTouch ? ev.touches[0].clientY - startPositon.y : ev.clientY - startPositon.y
 
-    //   // We cant calculate velocity from the first event
-    //   if (startPositon.x === 0 && startPositon.y === 0) {
-    //     dx = 0
-    //     dy = 0
-    //   }
+      // We cant calculate velocity from the first event
+      if (startPositon.x === 0 && startPositon.y === 0) {
+        dx = 0
+        dy = 0
+      }
 
-    //   const vx = -(dx - lastPosition.dx) / (lastPosition.timeStamp - Date.now())
-    //   const vy = -(dy - lastPosition.dy) / (lastPosition.timeStamp - Date.now())
+      const vx = -(dx - lastPosition.dx) / (lastPosition.timeStamp - Date.now())
+      const vy = -(dy - lastPosition.dy) / (lastPosition.timeStamp - Date.now())
 
-    //   const gestureState = { dx, dy, vx, vy, timeStamp: Date.now() }
-    //   return gestureState
-    // }
+      const gestureState = { dx, dy, vx, vy, timeStamp: Date.now() }
+      return gestureState
+    }
+
+    React.useLayoutEffect(() => {
+      let startPositon = { x: 0, y: 0 }
+      let lastPosition = { dx: 0, dy: 0, vx: 0, vy: 0, timeStamp: Date.now() }
+      let isClicking = false
+
+      element.current.addEventListener(('touchstart'), (ev) => {
+        if (!ev.srcElement.className.includes('pressable') && ev.cancelable) {
+          ev.preventDefault()
+        }
+
+        const gestureState = gestureStateFromWebEvent(ev, startPositon, lastPosition, true)
+        lastPosition = gestureState
+        startPositon = { x: ev.touches[0].clientX, y: ev.touches[0].clientY }
+      })
+
+      element.current.addEventListener(('mousedown'), (ev) => {
+        isClicking = true
+        const gestureState = gestureStateFromWebEvent(ev, startPositon, lastPosition, false)
+        lastPosition = gestureState
+        startPositon = { x: ev.clientX, y: ev.clientY }
+      })
+
+      const handleMove = (gestureState) => {
+        // Check fulfillment
+        if (onSwipeRequirementFulfilled || onSwipeRequirementUnfulfilled) {
+          const dir = getSwipeDirection({
+            x: swipeRequirementType === 'velocity' ? gestureState.vx : gestureState.dx,
+            y: swipeRequirementType === 'velocity' ? gestureState.vy : gestureState.dy
+          })
+          if (dir !== swipeThresholdFulfilledDirection) {
+            swipeThresholdFulfilledDirection = dir
+            if (swipeThresholdFulfilledDirection === 'none') {
+              if (onSwipeRequirementUnfulfilled) onSwipeRequirementUnfulfilled()
+            } else {
+              if (onSwipeRequirementFulfilled) onSwipeRequirementFulfilled(dir)
+            }
+          }
+        }
+
+        // use guestureState.vx / guestureState.vy for velocity calculations
+        // translate element
+        let rot = gestureState.vx * 15 // Magic number 15 looks about right
+        rot = Math.max(Math.min(rot, settings.maxTilt), -settings.maxTilt)
+        setSpringTarget.start({ xyrot: [gestureState.dx, gestureState.dy, rot], config: physics.touchResponsive })
+      }
+
+      window.addEventListener(('mousemove'), (ev) => {
+        if (!isClicking) return
+        const gestureState = gestureStateFromWebEvent(ev, startPositon, lastPosition, false)
+        lastPosition = gestureState
+        handleMove(gestureState)
+      })
+
+      window.addEventListener(('mouseup'), (ev) => {
+        if (!isClicking) return
+        isClicking = false
+        handleSwipeReleased(setSpringTarget, lastPosition)
+        startPositon = { x: 0, y: 0 }
+        lastPosition = { dx: 0, dy: 0, vx: 0, vy: 0, timeStamp: Date.now() }
+      })
+
+      element.current.addEventListener(('touchmove'), (ev) => {
+        const gestureState = gestureStateFromWebEvent(ev, startPositon, lastPosition, true)
+        lastPosition = gestureState
+        handleMove(gestureState)
+      })
+
+      element.current.addEventListener(('touchend'), (ev) => {
+        handleSwipeReleased(setSpringTarget, lastPosition)
+        startPositon = { x: 0, y: 0 }
+        lastPosition = { dx: 0, dy: 0, vx: 0, vy: 0, timeStamp: Date.now() }
+      })
+    })
 
     const element = React.useRef()
 
